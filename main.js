608b8592838dadb52b8206655f869b7ea92c8b96
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-01-29 17:37:43 +0100

    add/remove tasks and line-through

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        tasks: []
+        newTask: {
+            text: " ",
+            done: false
+        },
+        tasks: [
+            {
+                text: "Fare l'esercizio",
+                done: false
+            }
+        ]
       }
+    },
+    methods: {
+        addTask() {
+
+            if(this.newTask.text !== " "){
+                this.tasks.unshift(this.newTask)
+            }
+
+            this.newTask = {
+                text: " ",
+                done: false
+            }
+        },
+        taskDone(index) {
+            this.tasks[index].done = true
+        },
+        removeTask(index)  {
+            this.tasks.splice(index, 1)
+        }
     }
   }).mount('#app')
